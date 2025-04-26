4dc2b78abcda723ebfca46788aec89d5e7ea3eee
Anastasia Kostyuchenko <a-kostyuchenko@internet.ru>
2025-04-27 00:00:15 +0200

    project 10

---
+++ src/components/article-params-form/useModalClose.ts
@@ -0,0 +1,36 @@
+import { useEffect } from 'react';
+
+type UseModalClose = (params: {
+	containerRef: React.RefObject<HTMLElement>;
+	isOpened: boolean;
+	setClosed: () => void;
+}) => void;
+
+export const useModalClose: UseModalClose = ({
+	containerRef,
+	isOpened,
+	setClosed,
+}) => {
+	useEffect(() => {
+		if (!isOpened) return;
+
+		const handleEvent = (e: MouseEvent | KeyboardEvent) => {
+			if (e instanceof MouseEvent) {
+				if (containerRef.current && !containerRef.current.contains(e.target as Node)) {
+					setClosed();
+				}
+			}
+			if (e instanceof KeyboardEvent && e.key === 'Escape') {
+				setClosed();
+			}
+		};
+
+		window.addEventListener('mousedown', handleEvent);
+		window.addEventListener('keydown', handleEvent);
+
+		return () => {
+			window.removeEventListener('mousedown', handleEvent);
+			window.removeEventListener('keydown', handleEvent);
+		};
+	}, [isOpened, setClosed]);
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,33 @@
 import { createRoot } from 'react-dom/client';
 import { StrictMode, CSSProperties } from 'react';
 import clsx from 'clsx';
+import { useState } from 'react';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import { defaultArticleState, ArticleStateType } from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
-
+console.log(styles);
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
+const articleStateStyle = (articleState: ArticleStateType) => ({
+	'--font-family': articleState.fontFamilyOption.value,
+	'--font-size': articleState.fontSizeOption.value,
+	'--font-color': articleState.fontColor.value,
+	'--container-width': articleState.contentWidth.value,
+	'--bg-color': articleState.backgroundColor.value,
+});
+
 const App = () => {
+	const [articleStyleState, setArticleStyleState] = useState<ArticleStateType>(defaultArticleState);
 	return (
 		<main
 			className={clsx(styles.main)}
-			style={
-				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm />
+			style={articleStateStyle(articleStyleState) as CSSProperties}>
+			<ArticleParamsForm setArticleStyleState={setArticleStyleState} />
 			<Article />
 		</main>
 	);
